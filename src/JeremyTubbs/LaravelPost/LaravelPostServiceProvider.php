d716c5aa12be1f5fb9e33763ed24b5bc82a9cdcb
Jeremy Tubbs <jeremytubbs@gmail.com>
2015-01-29 09:11:56 -0500

    first commit

---
+++ src/JeremyTubbs/LaravelPost/LaravelPostServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace JeremyTubbs\LaravelPost;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPostServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('jeremy-tubbs/laravel-post');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
